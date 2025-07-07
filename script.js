fb0f75b250c617d9470155a3b96738d325fe0db3
hakenna <auroratoledo7777@gmail.com>
2025-07-06 22:51:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-document.getElementById("mensagemBtn").addEventListener("click", function() {
-  const mensagem = document.getElementById("mensagem");
-  mensagem.innerText = "Você está indo muito bem! Continue aprendendo. 🚀";
-  mensagem.style.textAlign = "center";
-  mensagem.style.marginTop = "15px";
-  mensagem.style.color = "#27ae60";
-  mensagem.style.fontWeight = "bold";
-});
+function mostrarMensagem() {
+  alert("Você clicou no botão! Parabéns pelo seu site 🎉");
+}

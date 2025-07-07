f13e3d9e18bc01f98e33d07854299238a50a19ae
hakenna <auroratoledo7777@gmail.com>
2025-07-06 22:41:29 -0400

    Update script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("mensagemBtn").addEventListener("click", function() {
+  const mensagem = document.getElementById("mensagem");
+  mensagem.innerText = "Você está indo muito bem! Continue aprendendo. 🚀";
+  mensagem.style.textAlign = "center";
+  mensagem.style.marginTop = "15px";
+  mensagem.style.color = "#27ae60";
+  mensagem.style.fontWeight = "bold";
+});
